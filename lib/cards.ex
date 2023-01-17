203307329ac6982d2d017d2b24a0ec3c73cc8113
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-17 22:28:53 +0100

    Update

--- lib/cards.ex
+++ lib/cards.ex
@@ -13,7 +13,7 @@ defmodule Cards do
     print_n_times(message, n-1)
   end
 
-  def print_n_times(message, n) do
+  def print_n_times(_message, _n) do
     :ok
   end
 
@@ -37,9 +37,31 @@ end
 defmodule Calc do
   def add(x,y) do
     if elem(x,0)==:num and elem(y,0)==:num do
-      tuple = {:num, elem(x,1)+elem(y,1)}
+      _tuple = {:num, elem(x,1)+elem(y,1)}
     else
-      tuple = {:num, 99}
+      _tuple = {:num, 99}
     end
+  end
+end
+
+defmodule Calculus do
+  @type literal() :: {:num, number()} | {:var, atom()}
+  @type expr() :: {:add, expr(), expr()} | {:mul, expr(), expr()} | literal()
+  #2x+3
+  #{:add, {:mul, {:num, 2}, {:var, :x}}, {:num, 3}}
+  def deriv({:num, _}, _) do
+    0
+  end
+  def deriv({:var, v}, v) do
+    1
+  end
+  def deriv({:var, _}, _) do
+    0
+  end
+  def deriv({:mul, e1, e2}, v) do
+    {:add, {:mul, deriv(e1,v), e2}, {:mul, deriv(e2,v), e1}}
+  end
+  def deriv({:add, e1, e2}, v) do
+    {:add, deriv(e1,v), deriv(e2,v)}
   end
 end
