baeece928efffad8625de7f180681305d9b694ff
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-17 23:30:52 +0100

    Update

--- lib/cards.ex
+++ lib/cards.ex
@@ -50,13 +50,13 @@ defmodule Calculus do
   #2x+3
   #{:add, {:mul, {:num, 2}, {:var, :x}}, {:num, 3}}
   def deriv({:num, _}, _) do
-    0
+    {:num, 0}
   end
   def deriv({:var, v}, v) do
-    1
+    {:num, 1}
   end
   def deriv({:var, _}, _) do
-    0
+    {:num, 0}
   end
   def deriv({:mul, e1, e2}, v) do
     {:add, {:mul, deriv(e1,v), e2}, {:mul, deriv(e2,v), e1}}
@@ -64,4 +64,47 @@ defmodule Calculus do
   def deriv({:add, e1, e2}, v) do
     {:add, deriv(e1,v), deriv(e2,v)}
   end
+
+  #Pure numbers or variables
+  def simplify({:num, a}) do
+    {:num, a}
+  end
+  def simplify({:var, a}) do
+    {:var, a}
+  end
+
+  #Addition
+  def simplify({:add, {:num, 0}, e}) do
+    simplify(e)
+  end
+  def simplify({:add, {:num, a}, {:num, b}}) do
+    {:num, a+b}
+  end
+  def simplify({:add, {:var, a}, {:var, b}}) do
+    {:add, {:var, a}, {:var, b}}
+  end
+  def simplify({:add, {:var, a}, {:num, b}}) do
+    {:add, {:var, a}, {:num, b}}
+  end
+  def simplify({:add, e1, e2}) do
+    simplify({:add, simplify(e1), simplify(e2)})
+  end
+
+  #Multiplication
+  def simplify({:mul, {:num, 0}, _}) do
+    {:num, 0}
+  end
+  def simplify({:mul, {:num, a}, {:num, b}}) do
+    {:num, a*b}
+  end
+  def simplify({:mul, {:var, a}, {:var, b}}) do
+    {:mul, {:var, a}, {:var, b}}
+  end
+  def simplify({:mul, {:num, a}, {:var, b}}) do
+    {:mul, {:num, a}, {:var, b}}
+  end
+  def simplify({:mul, e1, e2}) do
+    simplify({:mul, simplify(e1), simplify(e2)})
+  end
+
 end
