d2eb7f0f03fdb29ee3b3095f959f6ecc6b3889ef
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-17 18:14:14 +0100

    update

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,6 @@
 defmodule Cards do
   def create_deck do 
-    values = ["Ace","Two","Three", "Four", "Five"]
+    #values = ["Ace","Two","Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for suit <- suits do
@@ -17,13 +17,29 @@ defmodule Cards do
     :ok
   end
 
-
-
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
   def contains?(deck, hand) do
     Enum.member?(deck,hand)
+  end
+
+
+  def fib(0) do 0 end
+  def fib(1) do 1 end
+  def fib(n) do
+    fib(n-1)+fib(n-2)
+  end
+
+end
+
+defmodule Calc do
+  def add(x,y) do
+    if elem(x,0)==:num and elem(y,0)==:num do
+      tuple = {:num, elem(x,1)+elem(y,1)}
+    else
+      tuple = {:num, 99}
+    end
   end
 end
