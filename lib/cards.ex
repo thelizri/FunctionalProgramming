ae7e59c206dea26f4911a57e21fbc674e2b2aa4a
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-23 19:40:34 +0100

    Better solution to nested arrays

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,21 +3,9 @@ defmodule Cards do
     values = ["Ace","Two","Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    cards = for suit <- suits do
-      for value <- values do
+    for suit <- suits, value <- values do
         "#{value} of #{suit}"
-      end
     end
-    List.flatten(cards)
-  end
-
-  def print_n_times(message, n) when n > 0 do
-    IO.puts(message)
-    print_n_times(message, n-1)
-  end
-
-  def print_n_times(_message, _n) do
-    :ok
   end
 
   def shuffle(deck) do
@@ -28,25 +16,4 @@ defmodule Cards do
     Enum.member?(deck,hand)
   end
 
-
-  def fib(0) do 0 end
-  def fib(1) do 1 end
-  def fib(n) do
-    fib(n-1)+fib(n-2)
-  end
-
-  def test({:var, :a}) do
-    {:var, :a}
-  end
-
-end
-
-defmodule Calc do
-  def add(x,y) do
-    if elem(x,0)==:num and elem(y,0)==:num do
-      _tuple = {:num, elem(x,1)+elem(y,1)}
-    else
-      _tuple = {:num, 99}
-    end
-  end
 end
