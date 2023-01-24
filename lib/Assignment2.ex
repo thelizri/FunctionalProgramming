55f722263e674d74aed235903ff448feadc87efa
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-25 00:41:34 +0100

    Update

--- lib/Assignment2.ex
+++ lib/Assignment2.ex
@@ -111,3 +111,36 @@ defmodule EnvList do
 	end
 
 end 
+
+defmodule Benchmark do
+
+	def test(i) do
+		Enum.map(1..i, fn(_) -> :rand.uniform(i) end)
+		|> Enum.reduce(EnvList.new(), fn(e, list) -> EnvList.add(list, e, :foo) end)
+	end
+
+	def bench(i, n) do
+		seq = Enum.map(1..i, fn(_) -> :rand.uniform(i) end)
+		list = Enum.reduce(seq, EnvList.new(), fn(e, list) ->EnvList.add(list, e, :foo) end)
+
+		seq = Enum.map(1..n, fn(_) -> :rand.uniform(i) end)
+
+		{add, _} = :timer.tc(fn() -> Enum.each(seq, fn(e) -> EnvList.add(list, e, :foo) end) end)
+
+		{lookup, _} = :timer.tc(fn() -> Enum.each(seq, fn(e) -> EnvList.lookup(list, e) end) end)
+
+		{remove, _} = :timer.tc(fn() -> Enum.each(seq, fn(e) -> EnvList.remove(list, e) end) end)
+
+		{i, add, lookup, remove}
+	end
+
+	def bench(n) do
+		ls = [16,32,64,128,256,512,1024,2*1024,4*1024,8*1024]
+		:io.format("# benchmark with ~w operations, time per operation in us\n", [n])
+		:io.format("~6.s~12.s~12.s~12.s\n", ["n", "add", "lookup", "remove"])
+
+		#This line doesn't compile
+		#Enum.each(ls, fn (i) -> {i, tla, tll, tlr} = bench(i, n) :io.format("~6.w~12.2f~12.2f~12.2f\n", [i, tla/n, tll/n, tlr/n]) end)
+	end
+
+end
