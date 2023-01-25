7f8a29a9b548b7004dd9a9d3cb72f9d88d1bf56c
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-25 11:12:36 +0100

    Update

--- lib/AdventOfCode1.ex
+++ lib/AdventOfCode1.ex
@@ -3,41 +3,46 @@ defmodule AOC do
 	def read() do
 		{:ok, input} = File.read("lib/input2.txt")
 		content = String.split(input, "\n\n")
-		find_elf(0, 0, content)
+		find_elf(0, 0, 0, content)
 	end
 
-	def find_elf(pos, max, [head]) do
-		current_position = pos+1
-		integers_in_form_of_strings = String.split(head, "\n")
+	def find_elf(curpos, maxpos, max, [head]) do
+		current_position = curpos+1
+		integers_in_form_of_strings = String.split(head, "\r\n")
 		value = calc(integers_in_form_of_strings, 0)
-
+		IO.write("Value of position #{current_position} is #{value}")
 		if value > max do
 			current_position
 		else
-			pos
+			maxpos
 		end
 	end
 
-	def find_elf(pos, max, [head|rest]) do
-		current_position = pos+1
-		integers_in_form_of_strings = String.split(head, "\n")
+	def find_elf(curpos, maxpos, max, [head|rest]) do
+		current_position = curpos+1
+		integers_in_form_of_strings = String.split(head, "\r\n")
 		value = calc(integers_in_form_of_strings, 0)
-
+		IO.write("Value of position #{current_position} is #{value}")
+		
 		if value > max do
-			find_elf(current_position, value, rest)
+			find_elf(current_position, current_position, value, rest)
 		else
-			find_elf(pos, max, rest)
+			find_elf(current_position, maxpos, max, rest)
 		end
 	end
 
 	def calc([head], sum) do
-		x = String.to_integer(head)
-		sum+x
+		case head do
+			"" -> sum
+			_ -> x = String.to_integer(head); sum+x
+		end
 	end
 
 	def calc([head|rest], sum) do
-		x = String.to_integer(head)
-		calc(rest, sum+x)
+		case head do
+			"" -> sum
+			_ -> x = String.to_integer(head); calc(rest, sum+x)
+		end
 	end
 
 end
