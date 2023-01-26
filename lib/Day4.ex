c184e2f52b0d072471897cad2ddb65a95645265e
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-26 18:19:56 +0100

    Finished

--- lib/Day4.ex
+++ lib/Day4.ex
@@ -3,23 +3,37 @@ defmodule Day4 do
 	def read() do
 		{:ok, input} = File.read("lib/Day4.txt")
 		content = String.split(input, "\r\n")
-		for row <- content do
-			row
-		end
+		for_list(content, 0)
+	end
+
+	def for_list([head], score) do
+		valueForRow = evaluate_row(head)
+		score+valueForRow
+	end
+
+	def for_list([head|rest], score) do
+		valueForRow = evaluate_row(head)
+		for_list(rest, score+valueForRow)
 	end
 
 	def evaluate_row(row) do
 		[first, last] = String.split(row, ",")
 		first = eval_range(first)
 		last = eval_range(last)
-		{first, last}
+		
+		cond do 
+			MapSet.subset?(first, last) -> 1
+			MapSet.subset?(last, first) -> 1
+			true -> 0
+		end
 	end
 
 	def eval_range(seq) do
 		[first, last] = String.split(seq, "-")
 		first = String.to_integer(first)
 		last = String.to_integer(last)
-		Enum.to_list(first..last)
+		list = Enum.to_list(first..last)
+		MapSet.new(list)
 	end
 
 end
