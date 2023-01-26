4b8e25a3bd3c7f064c21d72a8ea2e1382e5df86e
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-26 21:31:32 +0100

    Update

--- lib/Day1.ex
+++ lib/Day1.ex
@@ -1,9 +1,10 @@
 defmodule Day1 do
 
 	def read() do
-		{:ok, input} = File.read("lib/input.txt")
+		{:ok, input} = File.read("lib/Day1.txt")
 		content = String.split(input, "\r\n\r\n")
-		find_elf(0, 0, 0, content)
+		max = top_3(content, {0,0,0})
+		sum(max)
 	end
 
 	def find_elf(_curpos, _maxpos, max, [head]) do
@@ -39,6 +40,32 @@ defmodule Day1 do
 			"" -> sum
 			_ -> x = String.to_integer(head); calc(rest, sum+x)
 		end
+	end
+
+	def top_3([head], max) do
+		ints = String.split(head, "\r\n")
+		value = calc(ints, 0)
+		updateMax(value, max)
+	end
+
+	def top_3([head|rest], max) do
+		ints = String.split(head, "\r\n")
+		value = calc(ints, 0)
+		max = updateMax(value, max)
+		top_3(rest, max)
+	end
+
+	def updateMax(value, {max1, max2, max3}) do
+		cond do
+			value > max1 -> {value, max1, max2}
+			value > max2 -> {max1, value, max2}
+			value > max3 -> {max1, max2, value}
+			true -> {max1, max2, max3}
+		end
+	end
+
+	def sum({max1, max2, max3}) do
+		max1+max2+max3
 	end
 
 end
