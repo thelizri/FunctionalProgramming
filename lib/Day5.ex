b7b32b8e1b6ebffe8f849cd8d1664fd6f75c55d1
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-27 14:04:26 +0100

    Update

--- lib/Day5.ex
+++ lib/Day5.ex
@@ -7,4 +7,20 @@ defmodule Day5 do
 		{from, to}
 	end
 
+	def read() do
+		{status, content} = File.read("lib/Day5.txt")
+		rows = String.split(content, "\r\n")
+		for row <- rows do
+			evaluate_row(row)
+		end
+	end
+
+	def evaluate_row(row) do
+		[_, amount, _, from, _, to] = String.split(row, " ")
+		amount = String.to_integer(amount)
+		from = String.to_integer(from)
+		to = String.to_integer(to)
+		{from, to, amount}
+	end
+
 end
