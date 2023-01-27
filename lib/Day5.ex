d30160b79c06dd88bba819abda006682be08eed2
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-27 15:02:13 +0100

    Finished Day5 Part1

--- lib/Day5.ex
+++ lib/Day5.ex
@@ -8,11 +8,12 @@ defmodule Day5 do
 	end
 
 	def read() do
-		{status, content} = File.read("lib/Day5.txt")
+		{_, content} = File.read("lib/Day5.txt")
 		rows = String.split(content, "\r\n")
-		for row <- rows do
+		commands = for row <- rows do
 			evaluate_row(row)
 		end
+		execute_program(commands, create_data())
 	end
 
 	def evaluate_row(row) do
@@ -21,6 +22,42 @@ defmodule Day5 do
 		from = String.to_integer(from)
 		to = String.to_integer(to)
 		{from, to, amount}
+	end
+
+	def execute_program([], data) do
+		present_result(data)
+	end
+
+	def execute_program([{from, to, amount}|tail], data) do
+		fromData = elem(data, from-1)
+		toData = elem(data, to-1)
+		{fromData, toData} = move(fromData, toData, amount)
+		data = put_elem(data, from-1, fromData)
+		data = put_elem(data, to-1, toData)
+		execute_program(tail, data)
+	end
+
+	def create_data() do
+		first = ["R", "W", "F", "H", "T", "S"]
+		second = ["W", "Q", "D", "G", "S"]
+		third = ["W", "T", "B"]
+		fourth = ["J", "Z", "Q", "N", "T", "W", "R", "D"]
+		fifth = ["Z", "T", "V", "L", "G", "H", "B", "F"]
+		sixth = ["G", "S", "B", "V", "C", "T", "P", "L"]
+		seventh = ["P", "G", "W", "T", "R", "B", "Z"]
+		eight = ["R", "J", "C", "T", "M", "G", "N"]
+		ninth = ["W", "B", "G", "L"]
+
+		{first, second, third, fourth, fifth, sixth, seventh, eight, ninth}
+	end
+
+	def present_result(data) do
+		size = tuple_size(data)
+		size = size-1
+		for colNum <- 0..size do
+			result = Enum.at(elem(data, colNum), 0)
+			IO.write("#{result}")
+		end
 	end
 
 end
