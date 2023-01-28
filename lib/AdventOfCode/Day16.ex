4c65869b52c72bce51dddd0e3be4add9256890a8
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-28 17:56:31 +0100

    Can Parse Day16. It's a start

--- lib/AdventOfCode/Day16.ex
+++ lib/AdventOfCode/Day16.ex
@@ -3,22 +3,21 @@ defmodule Day16 do
 	def read() do
 		{_, content} = File.read("lib/AdventOfCode/Day16.txt")
 		String.split(content, "\r\n") |>
-		evaluate_row
-
+		evaluate_row([])
 	end
 
-	def evaluate_row([]) do
-		nil
+	def evaluate_row([], results) do
+		Enum.reverse(results)
 	end
 
-	def evaluate_row([head|rest]) do
+	def evaluate_row([head|rest], results) do
 		nodes = Regex.scan(~r/\s*[A-Z]{2}\s*/, head)
 		|> List.flatten
 		[node|links] = for node <- nodes do
 			String.trim(node)
 		end
 		number = Regex.scan(~r/\d+/, head) |> List.flatten |> Enum.at(0) |> String.to_integer
-		{node, number, links}
+		evaluate_row(rest, [{node, number, links}|results])
 	end
 
 end
