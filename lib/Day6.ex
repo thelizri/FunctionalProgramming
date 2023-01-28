b3fb6a24ada8280d8dc338eea9cf48fe8143a8f2
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-28 13:34:19 +0100

    Update

--- lib/Day6.ex
+++ lib/Day6.ex
@@ -2,5 +2,39 @@ defmodule Day6 do
 
 	def read() do
 		{_status, content} = File.read("lib/Day6.txt")
-		content
+		list = String.to_charlist(content)
+		last14 = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]
+		processing(list, 1, last14)
 	end
+
+	def processing([], _, _) do
+		nil
+	end
+
+	def processing([head|tail], score, last14) do
+		last14 = push_and_pop(last14, head)
+		map = MapSet.new(last14)
+		if MapSet.size(map) > 13 do
+			score
+		else
+			processing(tail, score+1, last14)
+		end
+	end
+
+	def push(list, add) do
+		[add|list]
+	end
+
+	def pop([_last]) do
+		[]
+	end
+
+	def pop([head|tail]) do
+		[head|pop(tail)]
+	end
+
+	def push_and_pop(list, add) do
+		push(list, add) |> pop()
+	end
+
+end
