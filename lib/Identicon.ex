2fd5f7c7d08f1ae34463717daa94928fbf325e7a
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-28 01:29:19 +0100

    update

--- lib/Identicon.ex
+++ lib/Identicon.ex
@@ -3,11 +3,27 @@ defmodule Identicon do
 	def main(input) do
 		input
 		|> hash_input
+		|> pick_color
+		|> build_grid
 	end
 
 	def hash_input(input) do
 		:crypto.hash(:md5, input)
 		|> :binary.bin_to_list
 	end
+
+	def pick_color([r,g,b|tail]) do
+		{{r,g,b}, tail}
+	end
+
+	def build_grid({color, tail}) do
+		tail = Enum.chunk_every(tail, 3, 3, :discard) 
+		grid = for row <- tail do
+			mirror_row(row)
+		end
+		{color, grid}
+	end
+
+	def mirror_row([a,b,c]) do [a,b,c,b,a] end
 
 end
