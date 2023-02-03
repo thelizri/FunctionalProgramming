e28e04f8c0b74d39a5d020ef137319e6727b8ee7
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-03 17:19:42 +0100

    Update2

--- lib/Assignments/HigherOrderFunctions.ex
+++ lib/Assignments/HigherOrderFunctions.ex
@@ -12,6 +12,27 @@ defmodule HigherOrder do
 		end
 	end
 
-	
+	########################################################################################
+	# Without using list comprehensions
+
+	def double2(numbers) do
+		Enum.reverse(double2(numbers, []))
+	end
+
+	def double2([], accumulator) do accumulator end
+
+	def double2([head|rest], accumulator) do
+		double2(rest, [2*head|accumulator])
+	end
+
+	def five2(numbers) do
+		Enum.reverse(five2(numbers, []))
+	end
+
+	def five2([], accumulator) do accumulator end
+
+	def five2([head|rest], accumulator) do
+		five2(rest, [5+head|accumulator])
+	end
 
 end
