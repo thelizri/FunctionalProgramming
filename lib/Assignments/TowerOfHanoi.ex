6bdd27d20f7da7d0a28ab548057d8d3e0aba2434
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-03 18:10:11 +0100

    update

--- lib/Assignments/TowerOfHanoi.ex
+++ lib/Assignments/TowerOfHanoi.ex
@@ -2,9 +2,9 @@ defmodule Hanoi do
 	# Pegs are defined as :a, :b, :c
 	# Move is represented by a tuple. {:move, from, to}
 
-	def find_move(0, _, _, _) do [] end
-	def find_move(n, from, aux, to) do
-		find_move(n-1, from, to, aux) ++ [{:move, from, to}] ++ find_move(n-1, aux, from, to)
+	def hanoi(0, _, _, _) do [] end
+	def hanoi(n, from, aux, to) do
+		hanoi(n-1, from, to, aux) ++ [{:move, from, to}] ++ hanoi(n-1, aux, from, to)
 	end
 
 	# Algorithm for moving N from -> to, with aux peg
