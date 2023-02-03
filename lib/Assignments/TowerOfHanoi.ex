4871c8b4f12d88acd63fcf6ac62f15c07e9948c0
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-03 15:06:32 +0100

    Update

---
+++ lib/Assignments/TowerOfHanoi.ex
@@ -0,0 +1,10 @@
+defmodule Hanoi do
+	# Pegs are defined as :a, :b, :c
+	# Move is represented by a tuple. {:move, from, to}
+
+	def execute(size, firstPeg, secondPeg, lastPeg) do
+		
+	end
+
+
+end
