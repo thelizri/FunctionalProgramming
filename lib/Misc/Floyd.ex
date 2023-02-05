94dee285dc7ad8abcc6cc5fe92659fec71bfa229
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-05 03:29:41 +0100

    update

--- lib/Misc/Floyd.ex
+++ lib/Misc/Floyd.ex
@@ -25,4 +25,18 @@ defmodule Floyd do
 		IO.puts("  <- Value")
 		cond do j > 1 -> loopj(k, i, j-1, args, result); true -> result; end
 	end
+
+	# Floyd-Warshall Algorithm
+	# let V = number of vetrices in graph
+	# let dist = V * V array of minimum distances
+	# for each vertex v
+	# 	dist[v][v] <- 0
+	# for each edge (u,v)
+	# 	dist[u][v] <- weight(u,v)
+	# for k from 1 to V
+	# 	for i from 1 to V
+	# 		for j from 1 to V
+	# 			if dist[i][j] > dist[i][k] + dist[k][j]
+	#				dist[i][j] <- dist[i][k] + dist[k][j]
+	#			end if
 end
