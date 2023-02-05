6b6ac624b34ec80f718e2eca5116324487f01a49
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-05 01:25:08 +0100

    update

--- lib/AdventOfCode/Day16.ex
+++ lib/AdventOfCode/Day16.ex
@@ -23,4 +23,18 @@ defmodule Day16 do
 		evaluate_row(rest, [{node, number, links}|results])
 	end
 
+	# Floyd-Warshall Algorithm
+	# let V = number of vetrices in graph
+	# let dist = V * V array of minimum distances
+	# for each vertex v
+	# 	dist[v][v] <- 0
+	# for each edge (u,v)
+	# 	dist[u][v] <- weight(u,v)
+	# for k from 1 to V
+	# 	for i from 1 to V
+	# 		for j from 1 to V
+	# 			if dist[i][j] > dist[i][k] + dist[k][j]
+	#				dist[i][j] <- dist[i][k] + dist[k][j]
+	#			end if
+
 end
