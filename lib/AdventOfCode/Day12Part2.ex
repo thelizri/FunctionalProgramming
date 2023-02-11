f8fb709799a437a441ab05ac647c89048424b101
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-11 01:39:37 +0100

    Update

--- lib/AdventOfCode/Day12Part2.ex
+++ lib/AdventOfCode/Day12Part2.ex
@@ -25,4 +25,38 @@ defmodule Day12Part2 do
 		list = Enum.map(list, fn(x)-> case x do 83 -> 97; 69 -> 122; _ -> x; end end)
 	end
 
+	def getUp(index, {row, col}) do
+		index = index-col
+		cond do index < 0 -> :error; true -> index; end
+	end
+
+	def getDown(index, {row, col}) do
+		length=row*col;
+		index = index+col
+		cond do index >= length -> :error; true -> index; end
+	end
+
+	def getRight(index, {row, col}) do
+		index = index+1
+		cond do rem(index, col) == 0 -> :error; true -> index; end
+	end
+
+	def getLeft(index, {row, col}) do
+		cond do rem(index, col) == 0 -> :error; true -> index - 1; end
+	end
+
+	# Floyd-Warshall Algorithm
+	# let V = number of vetrices in graph
+	# let dist = V * V array of minimum distances
+	# for each vertex v
+	# 	dist[v][v] <- 0
+	# for each edge (u,v)
+	# 	dist[u][v] <- weight(u,v)
+	# for k from 1 to V
+	# 	for i from 1 to V
+	# 		for j from 1 to V
+	# 			if dist[i][j] > dist[i][k] + dist[k][j]
+	#				dist[i][j] <- dist[i][k] + dist[k][j]
+	#			end if
+
 end
