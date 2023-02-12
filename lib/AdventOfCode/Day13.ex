af3d1699b98ee77c7b332721f0504c3eeb54fc77
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-12 04:17:51 +0100

    update

--- lib/AdventOfCode/Day13.ex
+++ lib/AdventOfCode/Day13.ex
@@ -1,14 +1,47 @@
 defmodule Day13 do
 	def read() do
 		{:ok, content} = File.read("lib/AdventOfCode/Day13.txt")
-		String.split(content, "\r\n\r\n") |> checkBlock()
+		String.split(content, "\r\n\r\n") |> checkBlock(1, [])
 	end
 
-	def checkBlock([block|rest]) do
+	def checkBlock([], _, result) do IO.inspect(result); Enum.sum(result) end
+	def checkBlock([block|rest], index, result) do
 		[str1, str2] = String.split(block, "\r\n")
 		{list1, _} = Code.eval_string(str1)
 		{list2, _} = Code.eval_string(str2)
+		case compareRows(list1, list2) do
+			true -> checkBlock(rest, index+1, result ++ [index])
+			false -> checkBlock(rest, index+1, result)
+		end
 	end
+
+	def compareInts(first, second) when is_integer(first) and is_integer(second) do
+		first <= second
+	end
+
+	def compareRows([], []) do true end
+	def compareRows(head, []) do false end
+	def compareRows([], head) do true end
+	def compareRows([head1|rest1], [head2|rest2]) do
+		type = {is_integer(head1), is_integer(head2)}
+		result = case type do
+			{true, true} -> compareInts(head1, head2)
+			{true, false} -> compareRows(head1, [head2])
+			{false, true} -> compareRows([head1], head2)
+			{false, false} -> compareRows(head1, head2)
+		end
+		case result do
+			false -> false
+			true -> compareRows(rest1, rest2)
+		end
+	end
+	#def compareRows([head1|rest1], head2) do
+	#	compareRows([head1|rest1], [head2])
+	#end
+	#def compareRows(head1, [head2|rest2]) do
+	#	compareRows([head1], [head2|rest2])
+	#end
+	def compareRows(head1, head2) do head1 <= head2 end
 end
 
 # Rules
