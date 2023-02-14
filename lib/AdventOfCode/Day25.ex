2e7927b288bcbacb9c2377677200f4fd5554cf71
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-15 00:55:29 +0100

    Update

--- lib/AdventOfCode/Day25.ex
+++ lib/AdventOfCode/Day25.ex
@@ -3,7 +3,7 @@ defmodule Day25 do
 	def read() do
 		{:ok, content} = File.read("lib/AdventOfCode/Day25.txt")
 		list = String.split(content, "\r\n", trim: true)
-		getSum(list)
+		getSum(list) |> IO.inspect |> toSNAFU()
 	end
 
 	def getSum(list) do
@@ -28,8 +28,44 @@ defmodule Day25 do
 		result = Integer.to_string(rem(num, 5)) <> result
 		num = div(num, 5)
 		cond do
-			num < 5 -> Integer.to_string(num) <> result |> String.to_integer
+			num < 5 -> Integer.to_string(num) <> result
 			true -> base10To5(num, result)
+		end
+	end
+
+	# Decimal          SNAFU
+	#        1              1
+	#        2              2
+	#        3             1=
+	#        4             1-
+	#        5             10
+	#        6             11
+	#        7             12
+	#        8             2=
+	#        9             2-
+	def toSNAFU(num) do
+		list = base10To5(num) |> String.reverse() |>String.split("", trim: true)
+		toSNAFU(list, 0, "")
+	end
+
+	def toSNAFU([], carry, result) do
+		case carry do
+			0 -> result
+			1 -> "1" <> result
+			2 -> "2" <> result
+		end
+	end
+
+	def toSNAFU([num|rest], carry, result) do
+		num = String.to_integer(num)+carry
+		case num do
+			0 -> toSNAFU(rest, 0, "0" <> result)
+			1 -> toSNAFU(rest, 0, "1" <> result)
+			2 -> toSNAFU(rest, 0, "2" <> result)
+			3 -> toSNAFU(rest, 1, "=" <> result)
+			4 -> toSNAFU(rest, 1, "-" <> result)
+			5 -> toSNAFU(rest, 1, "0" <> result)
+			6 -> toSNAFU(rest, 1, "1" <> result)
 		end
 	end
 
