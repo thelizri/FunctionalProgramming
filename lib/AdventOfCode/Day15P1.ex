10db21568a5d1fc4caf59f986bc29e543c20cabf
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-16 15:59:05 +0100

    Update

--- lib/AdventOfCode/Day15P1.ex
+++ lib/AdventOfCode/Day15P1.ex
@@ -1,5 +1,48 @@
-defmodule Day15P2 do
+defmodule Day15P1 do
 
-	
+	def read() do
+		{:ok, content} = File.read("lib/AdventOfCode/Day15.txt")
+		list = String.split(content, "\r\n", trim: true) |> parse()
+		|> transform()
+		findSpot(list) |> IO.inspect
+		|> calcScore
+	end
+
+	def parse(list) do
+		parse_row = fn(head) -> Regex.scan(~r/-?\d+/, head) |> List.flatten() |> Enum.map(fn(x)->String.to_integer(x) end) end
+		Enum.map(list, fn(x)-> parse_row.(x) end)
+	end
+
+	def transform(list) do
+		transform_row = fn([sX, sY, bX, bY])-> d=abs(bX-sX)+abs(bY-sY); {sX, sY, d} end
+		Enum.map(list, fn(x)-> transform_row.(x) end)
+	end
+
+	def isInRange?({x, y}, {a,b,d}) do
+		dist = abs(a-x) + abs(b-y)
+		dist <= d
+	end
+
+	def checkIfInRangeOfAll?(pos, []) do false end
+	def checkIfInRangeOfAll?(pos, [head|rest]) do
+		case isInRange?(pos, head) do
+			true -> true
+			false -> checkIfInRangeOfAll?(pos, rest)
+		end
+	end
+
+	def findSpot(list) do
+		for i <- 0..20, j <- 0..20 do
+			bool=checkIfInRangeOfAll?({i,j},list)
+			case bool do
+				true -> nil
+				false -> {i, j}
+			end
+		end |> Enum.filter(fn(x) -> x != nil end) |> Enum.at(0)
+	end
+
+	def calcScore({x, y}) do
+		4000000*x + y
+	end
 
 end
