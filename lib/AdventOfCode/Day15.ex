f5bf4da5c4bc7ffff5d32c099b16c191bb42595b
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-16 03:53:50 +0100

    update

--- lib/AdventOfCode/Day15.ex
+++ lib/AdventOfCode/Day15.ex
@@ -1,4 +1,12 @@
 defmodule Day15 do
 
+	def read() do
+		{:ok, content} = File.read("lib/AdventOfCode/Day15.txt")
+		String.split(content, "\r\n", trim: true) |> parse()
+	end
 
+	def parse(list) do
+		parse_row = fn(head) -> Regex.scan(~r/-*\d+/, head) |> List.flatten() |> Enum.map(fn(x)->String.to_integer(x) end) end
+		Enum.map(list, fn(x)-> parse_row.(x) end)
+	end
 end
