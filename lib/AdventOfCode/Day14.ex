29802cb91d05083a9bd77a8fdbf3de0e38765959
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-17 14:59:30 +0100

    Update

--- lib/AdventOfCode/Day14.ex
+++ lib/AdventOfCode/Day14.ex
@@ -1,7 +1,25 @@
 defmodule Day14 do
 	def read() do
 		{:ok, content} = File.read("lib/AdventOfCode/Day14.txt")
-		content = String.split("\r\n", trim: true)
+		content = String.split(content, "\r\n", trim: true)
+		|> Enum.map(fn(row)-> parse_row(row) end)
+		|> Enum.reduce(MapSet.new(), fn(x, acc)-> transform_row(x, acc) end)
 	end
+
+	def parse_row(row) do
+		String.split(row, "->", trim: true)
+		|> Enum.map(fn(string)-> [x, y] = String.split(string, ",", trim: true); 
+			{String.trim(x)|>String.to_integer(), String.trim(y)|>String.to_integer()} end)
+	end
+
+	def transform_row([{x, y}, {a, b}|rest], map) do
+		res = cond do
+			x == a -> Enum.map(y..b, fn(n)->{x, n} end)
+			y == b -> Enum.map(x..a, fn(n)->{n, y} end)
+		end
+		|> Enum.reduce(map, fn(x, acc)-> MapSet.put(acc, x) end)
+		transform_row([{a,b}|rest], res)
+	end
+	def transform_row(_, map) do map end
 
 end
