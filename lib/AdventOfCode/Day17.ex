46c48476869336abb72818a7a403db6274021338
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-19 20:08:46 +0100

    Refactoring

--- lib/AdventOfCode/Day17.ex
+++ lib/AdventOfCode/Day17.ex
@@ -9,56 +9,41 @@ defmodule Day17 do
 		list = String.to_charlist(content) |> List.to_tuple
 		blizzards = {-1, list}
 		mapset = MapSet.new([{0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0}])
-		run_cycle(0, mapset, blizzards)
+		run(0, mapset, blizzards)
 	end
 
-	def run_cycle(num, mapset, blizzards) when num < 2022 do
+	def run(round, mapset, blizzards) when round < 2022 do
 		max_y = getTopMost(MapSet.to_list(mapset))
-		rock = getRock(num, max_y)
-		{mapset, blizzards} = move(rock, mapset, blizzards)
-		run_cycle(num+1, mapset, blizzards)
+		rock = getRock(round, max_y)
+		{mapset, blizzards} = move(round, rock, mapset, blizzards)
+		run(round+1, mapset, blizzards)
 	end
 
-	def run_cycle(_num, mapset, _blizzards) do
+	def run(_num, mapset, _blizzards) do
 		max_y = getTopMost(MapSet.to_list(mapset))
+	end
+
+	def move(round, rock, mapset, blizzards) do
+		{direction, blizzards} = getNext(blizzards)
+		newRock = case direction do
+			62 -> move_right(rock)
+			60 -> move_left(rock)
+		end
+		rock = case isOccupied(newRock, mapset) do
+			true -> rock
+			false -> newRock
+		end
+		newRock = move_down(rock)
+		case isOccupied(newRock, mapset) do
+			true -> {Enum.reduce(rock, mapset, fn(pos, acc)-> MapSet.put(acc, pos) end), blizzards}
+			false -> move(round, newRock, mapset, blizzards)
+		end
 	end
 
 	def getNext({index, list}) do
 		size=tuple_size(list)
 		index = rem(index+1, size)
 		{elem(list, index), {index, list}}
-	end
-
-	def getRock(round, max_y) when is_integer(round) do
-		round = rem(round, 5)
-		y = max_y+4
-		case round do
-			0 -> getRock(:row, y)
-			1 -> getRock(:plus, y)
-			2 -> getRock(:thel, y)
-			3 -> getRock(:column, y)
-			4 -> getRock(:square, y)
-		end
-	end
-
-	def getRock(:row, y) do
-		[{2, y},{3, y},{4, y},{5, y}]
-	end
-
-	def getRock(:plus, y) do
-		[{2, y+1},{3, y},{3, y+1},{3, y+2},{4, y+1}]
-	end
-
-	def getRock(:thel, y) do
-		[{2, y},{3, y},{4, y},{4, y+1}, {4, y+2}]
-	end
-
-	def getRock(:column, y) do
-		[{2, y},{2, y+1},{2, y+2},{2, y+3}]
-	end
-
-	def getRock(:square, y) do
-		[{2, y},{3, y},{2, y+1},{3, y+1}]
 	end
 
 	def getTopMost(list) do
@@ -73,47 +58,22 @@ defmodule Day17 do
 		Enum.reduce(list, -100, fn({x,y}, acc)-> max(x, acc) end)
 	end
 
-	def getBottomMost(list) do
-		Enum.reduce(list, nil, fn({x,y}, acc)-> case acc do
-			nil -> y; _ -> min(acc, y) end end)
-	end
-
-	def move(rock, mapset, blizzards) do
-		{direction, blizzards} = getNext(blizzards)
-		rock = case direction do
-			62 -> move_right(rock, mapset)
-			60 -> move_left(rock, mapset)
-		end
-		case move_down(Enum.map(rock, fn({x,y})->{x, y-1} end), mapset) do
-			true -> rock = Enum.map(rock, fn({x,y})->{x, y-1} end); move(rock, mapset, blizzards)
-			false -> {Enum.reduce(rock, mapset, fn(pos, acc)-> MapSet.put(acc, pos) end), blizzards}
-		end
-	end
-
-	def move_right(rock, mapset) do
-		r = getRightMost(rock)
-		rrock = Enum.map(rock, fn({x,y})->{x+1, y} end)
+	def move_left(rock) do
 		cond do
-			r < 6 and !isOccupied(rrock, mapset) -> rrock 
+			0 < getLeftMost(rock) -> Enum.map(rock, fn({x,y})->{x-1, y} end)
 			true -> rock
 		end
 	end
 
-	def move_left(rock, mapset) do
-		l = getLeftMost(rock)
-		lrock = Enum.map(rock, fn({x,y})->{x-1, y} end)
+	def move_right(rock) do
 		cond do
-			l > 0 and !isOccupied(lrock, mapset) -> lrock
+			getRightMost(rock) < 6 -> Enum.map(rock, fn({x,y})->{x+1, y} end)
 			true -> rock
 		end
 	end
 
-	def move_down([], mapset) do true end
-	def move_down([pos|rest], mapset) do
-		cond do
-			MapSet.member?(mapset, pos) -> false
-			true -> move_down(rest, mapset)
-		end
+	def move_down(rock) do
+		Enum.map(rock, fn({x,y})->{x, y-1} end)
 	end
 
 	def isOccupied([], mapset) do false end
@@ -124,5 +84,16 @@ defmodule Day17 do
 		end
 	end
 
+	def getRock(round, max_y) when is_integer(round) do
+		round = rem(round, 5)
+		y = max_y+4
+		case round do
+			0 -> [{2, y},{3, y},{4, y},{5, y}] # Row
+			1 -> [{2, y+1},{3, y},{3, y+1},{3, y+2},{4, y+1}] # Plus
+			2 -> [{2, y},{3, y},{4, y},{4, y+1}, {4, y+2}] # Backwards L
+			3 -> [{2, y},{2, y+1},{2, y+2},{2, y+3}] # Column
+			4 -> [{2, y},{3, y},{2, y+1},{3, y+1}] # Square
+		end
+	end
 
 end
