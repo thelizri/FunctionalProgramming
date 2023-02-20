50dc59e402b1f5632bb97cd4a5b75cbff6795137
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-20 07:16:19 +0100

    update

--- lib/AdventOfCode/Day18P2.ex
+++ lib/AdventOfCode/Day18P2.ex
@@ -9,7 +9,28 @@ defmodule Day18P2 do
 
 	def main() do
 		list = read()
-		Enum.reduce(list, 0, fn(x, acc)-> acc+getScore(x, list) end)
+		mapset = MapSet.new(list)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		mapset = floodAll(list, mapset)
+		getArea(MapSet.to_list(mapset))
 	end
 
 	#Returns -1 if neighbor. Otherwise returns 0
@@ -25,9 +46,43 @@ defmodule Day18P2 do
 		end
 	end
 
+	def getArea(list) do 
+		Enum.reduce(list, 0, fn(x, acc)-> acc+getScore(x, list)end)
+	end
+
 	def getScore(coord, list) do
 		Enum.reduce(list, 6, fn(x, acc)-> acc + isNeighbor(coord, x) end)
 	end
 
+	def isInside({x,y,z}, mapset) do
+		right = Enum.reduce(1..50, false, fn(n, acc)-> case acc do true -> true; false -> MapSet.member?(mapset, {x+n,y,z}) end end)
+		left = Enum.reduce(1..50, false, fn(n, acc)-> case acc do true -> true; false -> MapSet.member?(mapset, {x-n,y,z}) end end)
+		front = Enum.reduce(1..50, false, fn(n, acc)-> case acc do true -> true; false -> MapSet.member?(mapset, {x,y+n,z}) end end)
+		behind = Enum.reduce(1..50, false, fn(n, acc)-> case acc do true -> true; false -> MapSet.member?(mapset, {x,y-n,z}) end end)
+		top = Enum.reduce(1..50, false, fn(n, acc)-> case acc do true -> true; false -> MapSet.member?(mapset, {x+n,y,z+n}) end end)
+		bottom = Enum.reduce(1..50, false, fn(n, acc)-> case acc do true -> true; false -> MapSet.member?(mapset, {x-n,y,z-n}) end end)
+		case {right, left, front, behind, top, bottom} do
+			{true, true, true, true, true, true} -> true
+			_ -> false
+		end
+	end
+
+	def floodFill({x,y,z}, mapset) do
+		cond do
+			isInside({x,y,z}, mapset) -> MapSet.put(mapset, {x,y,z})
+			true -> mapset
+		end
+	end
+
+	def floodAll([], mapset) do mapset end
+	def floodAll([rock|rest], mapset) do
+		adj = getAdj(rock)
+		mapset = Enum.reduce(adj, mapset, fn(rrock, acc)->case MapSet.member?(mapset, rrock) do true -> acc; false -> floodFill(rrock, acc) end end)
+		floodAll(rest, mapset)
+	end
+
+	def getAdj({x,y,z}) do
+		[{x+1,y,z}, {x-1,y,z}, {x,y+1,z}, {x,y-1,z}, {x,y,z+1}, {x,y,z-1}]
+	end
 
 end
