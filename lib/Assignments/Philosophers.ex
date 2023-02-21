55173bb01fc1a766677c729e4b5b7b9216659e62
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-21 13:37:32 +0100

    Update

--- lib/Assignments/Philosophers.ex
+++ lib/Assignments/Philosophers.ex
@@ -1,6 +1,7 @@
 #spawn                   pid = spawn(fn-> :gurka end)
 #send/2                  send(pid, message)
 #receive				 receive do pattern -> :gurka; pattern2 -> :gurka2; end
+#receive will wait if the mailbox is empty or there is no matching pattern
 #I can have multiple recieve in different functions. When a message is sent, it's put in a bucket that belongs to my process.
 #
 
@@ -78,3 +79,30 @@ defmodule Chopstick do
 		send(stick, :quit)
 	end
 end
+
+defmodule Philosopher do
+
+	def start(left, right, hunger, name, ctrl) do
+		spawn_link(fn -> 
+			Enum.each(1..hunger, fn(n)-> sleep(1000); eat(left, right, name) end)
+			send(ctrl, :done)
+			end)
+	end
+
+	def sleep(0) do :ok end
+	def sleep(t) do
+		:timer.sleep(:rand.uniform(t))
+	end
+
+	def eat(left, right, name) do
+		Chopstick.request(left)
+		receive do
+			:ok -> IO.puts("#{name} received left chopstick!")
+		end
+		Chopstick.request(right)
+		receive do
+			:ok -> IO.puts("#{name} received right chopstick!")
+		end
+	end
+end
+
