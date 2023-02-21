02726aba12e34a91fcbcd4d00ea1031efef29f5a
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-21 12:44:13 +0100

    Update

--- lib/Assignments/Philosophers.ex
+++ lib/Assignments/Philosophers.ex
@@ -1,3 +1,10 @@
+#spawn                   pid = spawn(fn-> :gurka end)
+#send/2                  send(pid, message)
+#receive				 receive do pattern -> :gurka; pattern2 -> :gurka2; end
+#I can have multiple recieve in different functions. When a message is sent, it's put in a bucket that belongs to my process.
+#
+
+
 defmodule Storage do
 
 	def run() do
@@ -7,13 +14,33 @@ defmodule Storage do
 	defp loop(map) do
 	    receive do
 	    	{:get, key, caller} ->
-	        	send caller, Map.get(map, key); loop(map)
+	        	send(caller, {key, Map.get(map, key)}); loop(map)
 	    	{:put, key, value} -> 
 	    		loop(Map.put(map, key, value))
 	    end
   	end
 
-end 
+end
+
+defmodule ConPro do
+
+	def main do
+		Storage.run()
+	end
+
+	def store(pid, key, value) do
+		send(pid, {:put, key, value})
+	end
+
+	def get(pid, key) do
+		send(pid, {:get, key, self()})
+		receive do
+			{key, value} -> IO.inspect(value)
+			something -> IO.inspect(something)
+		end
+	end
+
+end
 
 defmodule Chopstick do
 
