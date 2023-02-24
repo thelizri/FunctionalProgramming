d382d832c95e5d9c73f0ccf3c4d6362bc81b5709
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-24 20:44:35 +0100

    update

---
+++ lib/AdventOfCode/Day16-20/Day19.ex
@@ -0,0 +1,40 @@
+defmodule Day19 do
+
+	def read() do
+		{:ok, content} = File.read("lib/AdventOfCode/Day16-20/Day19.txt")
+		String.split(content, "\r\n\r\n", trim: true)
+		|> Enum.map(fn(x)-> String.split(x, "\r\n", trim: true) end)
+		|> List.flatten()
+	end
+
+	def parse([a, b, c, d, e|rest]) do
+		map = Map.new()
+
+		[[num]] = Regex.scan(~r/\d+/, a)
+		blueprint_id = String.to_integer(num) 
+		map = Map.put(map, :id, blueprint_id)
+
+		[[num]] = Regex.scan(~r/\d+/, b)
+		ore_robot_cost = String.to_integer(num)
+		map = Map.put(map, :oreRobot, ore_robot_cost)
+
+		[[num]] = Regex.scan(~r/\d+/, c)
+		clay_robot_cost = String.to_integer(num) 
+		map = Map.put(map, :clayRobot, clay_robot_cost)
+
+		[[ore], [clay]] = Regex.scan(~r/\d+/, d)
+		ore = String.to_integer(ore)
+		clay = String.to_integer(clay)
+		map = Map.put(map, :obsidianRobot, [ore: ore, clay: clay])
+
+		[[ore], [obsidian]] = Regex.scan(~r/\d+/, e)
+		ore = String.to_integer(ore)
+		obsidian = String.to_integer(obsidian)
+		map = Map.put(map, :geodeRobot, [ore: ore, obsidian: obsidian])
+	end
+
+	def main() do
+		read() |> parse()
+	end
+
+end
