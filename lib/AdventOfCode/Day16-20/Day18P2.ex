64cea32d8f7cf14df177e52794bd4b51abd40b5b
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-24 02:22:12 +0100

    Depression

--- lib/AdventOfCode/Day16-20/Day18P2.ex
+++ lib/AdventOfCode/Day16-20/Day18P2.ex
@@ -40,29 +40,6 @@ defmodule Day18P2 do
 		|> Enum.count(fn(x)-> x end)
 	end
 
-	def getNeighbors({x,y,z}, visited) do
-		neighbours = [{x+1,y,z}, {x,y+1,z}, {x,y,z+1}, {x-1,y,z}, {x,y-1,z}, {x,y,z-1}]
-		Enum.filter(neighbours, fn({x,y,z})-> x<=@mymaxbound and x>=@myminbound 
-			and y<=@mymaxbound and y>=@myminbound and z<=@mymaxbound and z>=@myminbound end)
-		|> Enum.filter(fn({x,y,z})-> !MapSet.member?(visited, {x,y,z}) end)
-	end
-
-
-	#BFS (G, s)                   //Where G is the graph and s is the source node
-    #  let Q be queue.
-    #  Q.enqueue( s ) //Inserting s in queue until all its neighbour vertices are marked.
-	#
-    #  mark s as visited.
-    #  while ( Q is not empty)
-    #       //Removing that vertex from queue,whose neighbour will be visited now
-    #       v  =  Q.dequeue( )
-	#
-    #      //processing all the neighbours of v  
-    #      for all neighbours w of v in Graph G
-    #           if w is not visited 
-    #                    Q.enqueue( w )             //Stores w in Q to further visit its neighbour
-    #                    mark w as visited.
-
     #We will use depth first search to calculate the score
     def dfs({x,y,z}, mapset, visited) when x > @mymaxbound or x < @myminbound do true end
     def dfs({x,y,z}, mapset, visited) when y > @mymaxbound or y < @myminbound do true end
