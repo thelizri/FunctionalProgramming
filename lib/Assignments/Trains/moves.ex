62fe904ae0728f0dfcaec7d51f37b4e5355d2396
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-27 23:07:18 +0100

    Sequence done

--- lib/Assignments/Trains/moves.ex
+++ lib/Assignments/Trains/moves.ex
@@ -3,13 +3,19 @@ defmodule Moves do
 	# single/2 move state
 	# single({:one,1},{[:a,:b],[],[]})
 	def single({_, 0}, state) do state end
+	
+	def single({:one, n}, {[], one, two}) when n < 0 do
+		{Train.take(one, -n), Train.drop(one, -n), two}
+	end
 	def single({:one, n}, {main, one, two}) when n < 0 do
-		{one, main, two} = single({:one, -n}, {one, main, two})
-		{main, one, two}
+		{Train.append(main, Train.take(one, -n)), Train.drop(one, -n), two}
+	end
+
+	def single({:two, n}, {[], one, two}) when n < 0 do
+		{Train.take(two, -n), one, Train.drop(two, -n)}
 	end
 	def single({:two, n}, {main, one, two}) when n < 0 do
-		{two, one, main} = single({:two, -n}, {two, one, main})
-		{main, one, two}
+		{Train.append(main, Train.take(two, -n)), one, Train.drop(two, -n)}
 	end
 
 	def single({:one, n}, {main, [], two}) when n > 0 do
@@ -28,6 +34,18 @@ defmodule Moves do
 	def single({:two, n}, {main, one, two}) when n > 0 do
 		{k, remain, take} = Train.main(main, n)
 		{remain, one, Train.append(take, two)}
+	end
+
+	# sequence
+	def sequence(moves, state) do
+		sequence(moves, state, [state])
+	end
+
+	def sequence([], _, result) do result end
+	def sequence([move|rest], state, result) do
+		state = single(move, state)
+		result = Train.append(result, state)
+		sequence(rest, state, result)
 	end
 
 end
