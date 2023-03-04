faf8aff1522522d186baddec300c4aef105a7c6d
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-04 19:46:34 +0100

    Update

--- lib/Assignments/Huffman/Huffman.ex
+++ lib/Assignments/Huffman/Huffman.ex
@@ -16,10 +16,10 @@ defmodule Huffman do
 		sample = sample()
 		tree = tree(sample)
 		encode = encode_table(tree)
-		decode = decode_table(tree)
-		text = text()
-		seq = encode(text, encode)
-		decode(seq, decode)
+		#decode = decode_table(tree)
+		#text = text()
+		#seq = encode(text, encode)
+		#decode(seq, decode)
 	end
 
 	def frequency(text) do frequency(text, Map.new()) end
@@ -34,15 +34,29 @@ defmodule Huffman do
 		freq = frequency(sample)
 		|> Map.to_list()
 		|> Enum.sort(fn({_, f1}, {_, f2})-> f1 <= f2 end)
+		|> huffman()
 	end
 
-	def huffman() do
-
+	def huffman([{tree, _}]) do tree end
+	def huffman([{a, f1}, {b, f2}|rest]) do
+		[{{a, b}, f1+f2}|rest] 
+		|> Enum.sort(fn({_, f1}, {_, f2})-> f1 <= f2 end) #inefficient, but works for now
+		|> huffman()
 	end
 
 	# create an encoding table containing the mapping from characters to codes given a Huffman tree
-	def encode_table(tree) do
-		# To implement...
+	def encode_table({left, right}) do
+		map = encode_table(left, Map.new(), '0')
+		encode_table(right, map, '1')
+	end
+
+	def encode_table({left, right}, map, code) do
+		map = encode_table(left, map, '0'++code)
+		encode_table(right, map, '1'++code)
+	end
+
+	def encode_table(thenode, map, code) do
+		Map.put(map, to_string(code), List.to_string([thenode]))
 	end
 
 	# create an decoding table containing the mapping from codes to characters given a Huffman tree
