7cbc73796b6bcf42f15a8b7e89c1de0596914dab
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-06 17:23:51 +0100

    Bench

--- lib/Assignments/Huffman/Huffman.ex
+++ lib/Assignments/Huffman/Huffman.ex
@@ -27,8 +27,8 @@ defmodule Huffman do
 	end
 
 	# create a Huffman tree given a sample text
-	def tree(sample) do
-		freq = frequency(sample)
+	def tree(text) do
+		freq = frequency(text)
 		|> Map.to_list()
 		|> Enum.sort(fn({_, f1}, {_, f2})-> f1 <= f2 end)
 		|> huffman_tree()
@@ -86,6 +86,52 @@ defmodule Huffman do
 			decode(rest, table, [temp]++result, '')
 		else 
 			decode(rest, table, result, key)
+		end
+	end
+
+	# This is the benchmark of the single operations in the
+	# Huffman encoding and decoding process.
+
+	def bench(n) do
+		{text, b} = read("lib/Assignments/Huffman/kallocain.txt", n)
+		c = length(text)
+		{tree, t2} = time(fn -> tree(text) end)
+		{encode, t3} = time(fn -> encode_tree(tree) end)
+		s = map_size(encode)
+		{decode, _} = time(fn -> decode_table(encode) end)
+		{encoded, t5} = time(fn -> encode(text, encode) end)
+		e = div(length(encoded), 8)
+		r = Float.round(e / b, 3)
+		{_, t6} = time(fn -> decode(encoded, decode) end)
+
+		IO.puts("text of #{c} characters")
+		IO.puts("tree built in #{t2} ms")
+		IO.puts("table of size #{s} in #{t3} ms")
+		IO.puts("encoded in #{t5} ms")
+		IO.puts("decoded in #{t6} ms")
+		IO.puts("source #{b} bytes, encoded #{e} bytes, compression #{r}")
+	end
+
+	# Measure the execution time of a function.
+	def time(func) do
+		initial = Time.utc_now()
+		result = func.()
+		final = Time.utc_now()
+		{result, Time.diff(final, initial, :microsecond) / 1000}
+	end
+
+	# Get a suitable chunk of text to encode.
+	def read(file, n) do
+		{:ok, fd} = File.open(file, [:read, :utf8])
+		binary = IO.read(fd, n)
+		File.close(fd)
+
+		length = byte_size(binary)
+		case :unicode.characters_to_list(binary, :utf8) do
+		  {:incomplete, chars, rest} ->
+		    {chars, length - byte_size(rest)}
+		  chars ->
+		    {chars, length}
 		end
 	end
 
